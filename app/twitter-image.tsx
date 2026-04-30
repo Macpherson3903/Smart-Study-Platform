15910199a46849d0fb0d73bd6979cd04016ddad4
Macpherson3903 <109850340+Macpherson3903@users.noreply.github.com>
2026-04-30 12:32:02 +0100

    Refactor Twitter image export to utilize OpenGraphImage component; define metadata properties for improved social media previews.

--- app/twitter-image.tsx
+++ app/twitter-image.tsx
@@ -1,2 +1,13 @@
-export { alt, contentType, runtime, size } from "./opengraph-image";
-export { default } from "./opengraph-image";
+import OpenGraphImage from "./opengraph-image";
+
+export const runtime = "edge";
+export const alt = "Smart Study Platform social preview";
+export const size = {
+  width: 1200,
+  height: 630,
+};
+export const contentType = "image/png";
+
+export default function TwitterImage() {
+  return OpenGraphImage();
+}
